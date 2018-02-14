e31ba27b94e9603f33fa1f8f2162afeb044d5e73
Alexandre Petrachini <36488039+Alexandre-Petrachini@users.noreply.github.com>
2018-02-14 19:00:38 -0200

    Add files via upload

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,243 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* listitem  List item */
+    
+    
+        /* listitem  List item */
+    $(document).on("click", ".uib_w_5", function(evt)
+    {
+        /* your code goes here */ 
+    });
+    
+        /* listitem  List item */
+    $(document).on("click", ".uib_w_4", function(evt)
+    {
+         activate_subpage("#page_86_89"); 
+    });
+    
+        /* listitem  List item */
+    $(document).on("click", ".uib_w_5", function(evt)
+    {
+         activate_subpage("#page_86_89"); 
+    });
+    
+        /* listitem  List item */
+    $(document).on("click", ".uib_w_6", function(evt)
+    {
+         activate_subpage("#page_86_89"); 
+    });
+    
+        /* button  LPL */
+    
+    
+        /* button  LPOO */
+    
+    
+        /* button  DS */
+    
+    
+        /* button  Sair */
+    $(document).on("click", ".uib_w_15", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_7"));  
+    });
+    
+        /* button  Sair */
+    $(document).on("click", ".uib_w_16", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_13"));  
+    });
+    
+        /* button  Sair */
+    $(document).on("click", ".uib_w_17", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_14"));  
+    });
+    
+        /* button  Introdução */
+    $(document).on("click", ".uib_w_18", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_19"));  
+    });
+    
+        /* button  Sair */
+    $(document).on("click", ".uib_w_20", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_19"));  
+    });
+    
+        /* button  LPOO */
+    
+    
+        /* button  LPOO */
+    
+    
+        /* button  LPOO */
+    $(document).on("click", ".uib_w_11", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_21"));  
+    });
+    
+        /* button  Sair */
+    $(document).on("click", ".uib_w_22", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_21"));  
+    });
+    
+        /* button  DS */
+    $(document).on("click", ".uib_w_12", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_23"));  
+    });
+    
+        /* button  Sair */
+    $(document).on("click", ".uib_w_24", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_23"));  
+    });
+    
+        /* button  PC */
+    $(document).on("click", ".uib_w_25", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_26"));  
+    });
+    
+        /* button  Sair */
+    $(document).on("click", ".uib_w_27", function(evt)
+    {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_26"));  
+    });
+    
+        /* button  Sair */
+    
+    
+        /* button  Sair */
+    $(document).on("click", ".uib_w_38", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($("#sidebar4"));  
+         return false;
+    });
+    
+        /* button  Mod. III */
+    
+    
+        /* button  Mod. III */
+    
+    
+        /* button  Mod. III */
+    
+    
+        /* button  Mod. III */
+    
+    
+        /* button  Mod. III */
+    $(document).on("click", ".uib_w_10", function(evt)
+    {
+         /*global uib_sb */
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($("#sidebar1"));  
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
